13179f2da8faba7e920f22a0e8a073a26f3d8a14
gerardo antonicelli <esteban.antonicelli@gmail.com>
2020-08-17 01:40:47 +0200

    dataset documentation added

---
+++ R/data.R
@@ -0,0 +1,110 @@
+#' Production and farm value of maple products in Canada
+#'
+#' @source Statistics Canada. Table 001-0008 - Production and farm value of
+#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
+#' @format A data frame with columns:
+#' \describe{
+#' \item{Year}{A value between 1924 and 2015.}
+#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
+#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
+#' \item{Region}{Postal code abbreviation for territory or province.}
+#' }
+#' [1] "nb"  "nam" "seq" "com"
+#' @examples
+#' \dontrun{
+#' maple
+#' }
+'clustalRNA'
+
+#' Production and farm value of maple products in Canada
+#'
+#' @source Statistics Canada. Table 001-0008 - Production and farm value of
+#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
+#' @format A data frame with columns:
+#' \describe{
+#' \item{Year}{A value between 1924 and 2015.}
+#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
+#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
+#' \item{Region}{Postal code abbreviation for territory or province.}
+#' }
+#'  [1] "QHD43416.1"     "AAP30030.1"     "QHR63300.2"     "AGZ48831.1"     "YP_001039971.1" "ALK02457.1"     "AVP78031.1"     "AVP78042.1"     "AAY88866.1"     "AGZ48818.1"
+#'  [11] "AAS00003.1"     "AAP51227.1"     "AAU04646.1"     "YP_003858584.1"
+#' @examples
+#' \dontrun{
+#' maple
+#' }
+'fastaRNA'
+
+#' Production and farm value of maple products in Canada
+#'
+#' @source Statistics Canada. Table 001-0008 - Production and farm value of
+#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
+#' @format A data frame with columns:
+#' \describe{
+#' \item{Year}{A value between 1924 and 2015.}
+#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
+#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
+#' \item{Region}{Postal code abbreviation for territory or province.}
+#' }
+#' [1] "nb"  "nam" "seq" "com"
+#' @examples
+#' \dontrun{
+#' maple
+#' }
+'maseProtein'
+
+#' Production and farm value of maple products in Canada
+#'
+#' @source Statistics Canada. Table 001-0008 - Production and farm value of
+#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
+#' @format A data frame with columns:
+#' \describe{
+#' \item{Year}{A value between 1924 and 2015.}
+#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
+#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
+#' \item{Region}{Postal code abbreviation for territory or province.}
+#' }
+#'  [1] "QHD43415_1"     "AAP30028_1"     "QHR63299_1"     "AGZ48830_1"     "YP_001039970_1" "ALK02468_1"     "AVP78030_1"     "AVP78041_1"     "AAY88865_2"     "AGZ48817_1"
+#'  [11] "AAS00002_1"     "AAP51225_1"     "AAU04645_1"     "YP_003858583_1"
+#' @examples
+#' \dontrun{
+#' maple
+#' }
+'msfRNA'
+
+#' Production and farm value of maple products in Canada
+#'
+#' @source Statistics Canada. Table 001-0008 - Production and farm value of
+#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
+#' @format A data frame with columns:
+#' \describe{
+#' \item{Year}{A value between 1924 and 2015.}
+#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
+#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
+#' \item{Region}{Postal code abbreviation for territory or province.}
+#' }
+#' [1] "nb"  "nam" "seq" "com"
+#' @examples
+#' \dontrun{
+#' maple
+#' }
+'phylipProt'
+
+#' Production and farm value of maple products in Canada
+#'
+#' @source Statistics Canada. Table 001-0008 - Production and farm value of
+#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
+#' @format A data frame with columns:
+#' \describe{
+#' \item{Year}{A value between 1924 and 2015.}
+#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
+#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
+#' \item{Region}{Postal code abbreviation for territory or province.}
+#' }
+#'  [1] "QHD43416.1" "AAP30030.1" "QHR63300.2" "AGZ48831.1" "YP_0010399" "ALK02457.1" "AVP78031.1" "AVP78042.1" "AAY88866.1" "AGZ48818.1" "AAS00003.1" "AAP51227.1" "AAU04646.1"
+#'  [14] "YP_0038585"
+#' @examples
+#' \dontrun{
+#' maple
+#' }
+'phylipRNA'
