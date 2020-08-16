0154187dce3a2b11131c0d32b922fadc0bc7b966
gerardo antonicelli <esteban.antonicelli@gmail.com>
2020-08-16 15:36:47 +0200

    seqinr xref problem persist

--- R/functions.R
+++ R/functions.R
@@ -13,12 +13,12 @@
 #' @param seqnames a character vector specifying the accession numbers of the
 #'   sequences to be retrieved
 #' @param acnucdb a character string specifying the name of the ACNUC database
-#'   to be searched. Use \code{'\link[seqinr]{choosebank}'} without arguments to
+#'   to be searched. Use \code{'\link{choosebank}'} without arguments to
 #'   see a list of available databases
 #'
 #' @return this function returns a named list with the sequences matching the
 #'   sequence names of class SeqAcnucWeb retrieved by
-#'   \code{'\link[seqinr]{query}'} based on the provided accession numbers the
+#'   \code{'\link{query}'} based on the provided accession numbers the
 #'   name of each list item is composed of the accession number and the length
 #'   of the sequence
 #'
@@ -30,7 +30,9 @@
 #'
 #' @examples
 #' seqs <- retrieve_seqs(c('P06747', 'P0C569', 'O56773', 'Q5VKP1'), 'swissprot')
-#' \dontrun{viruses_annot <- read.csv(system.file('extdata', 'CoV_genomes_annot.csv', package='firstPackage'))}
+#' \dontrun{viruses_annot <- read.csv(system.file('extdata',
+#'                                                'CoV_genomes_annot.csv',
+#'                                                package='firstPackage'))}
 #' \dontrun{accessions <- viruses_annot$accession}
 #' \dontrun{seqs <- retrieve_seqs(accessions, 'genbank')}
 #' \dontrun{str(seqs)}
@@ -201,7 +203,7 @@ clean_alignment <- function(alignment, minpcnongap, minpcid){
 #'   alignment software
 #'
 #' this function is wrapper around the functions
-#'   \code{'\link[seqinr]{read.alignment}'},
+#'   \code{'\link{read.alignment}'},
 #'   \code{'\link[adegenet]{fasta2DNAbin}'} and \code{'\link[ape]{as.DNAbin}'}.
 #'   The goal of this function is to load different types of sequences alignments,
 #'   i.e. protein, DNA, RNA, in different formats, i.e. fasta, phylip, mase,
