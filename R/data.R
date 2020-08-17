607a1d804f3450d0b80b21339a7246ec4be26a71
gerardo antonicelli <esteban.antonicelli@gmail.com>
2020-08-17 05:41:35 +0200

    new dataset documentation added

--- R/data.R
+++ R/data.R
@@ -1,125 +1,113 @@
 #' alignment of RNA sequences for the spike protein of some sarbecoviruses
-#' closely related to SARSCoV2
+#'   closely related to SARSCoV2
 #'
+#' @description
 #' 14 different sequences of some sarbecoviruses closely related to SARSCoV2
-#' were retrieve from public databases and compared using CLUSTAL omega (1.2.4)
-#' alignment software (see examples section for the complete protocol for
-#' retrieving, saving and processing the data)
+#'   were retrieved from public databases and compared using CLUSTAL omega (1.2.4)
+#'   alignment software with the output in clustal format (see examples section for
+#'   the complete protocol for retrieving, saving and processing the data)
 #'
-#' @source the \href{https://www.ncbi.nlm.nih.gov/genbank/}{genbank} database of
+#' @source
+#'   the \href{https://www.ncbi.nlm.nih.gov/genbank/}{genbank} database of
 #'   the National Center for Biotechnology Information or the Universal Protein
 #'   Resource \href{https://www.uniprot.org/}{UniProt} database
 #'
-#' @format a list with 4 elements \describe{ \item{nb}{number of sequences
-#'   compared} \item{nam}{sequence name (accession number)} \item{seq}{a list of
-#'   RNA sequences} \item{com}{NA} }
+#' @format a list of class alignment with 4 elements
+#' \describe{\item{nb}{number of sequences compared}
+#'           \item{nam}{sequence name (accession number)}
+#'           \item{seq}{a list of RNA sequences}
+#'           \item{com}{NA}}
 #'
-#' @examples
-#' \dontrun{spikeRNA_annot <- read.csv(system.file('extdata',
-#'                                                'spike_annot.csv',
-#'                                                package='firstPackage'))}
-#' \dontrun{accessions <- spikeRNA_annot$accession}
-#' \dontrun{download manually each sequence from 'genbank' and save as spike.fasta}
-#' \dontrun{perform an alignment with clustal omega with following commands
-#'   (it needs to be installed locally or use a web interface)}
-#' \dontrun{clustalo --threads=4 -v --outfmt=clustal -t RNA -i
-#'          ./data/sequences/spike.fasta -o ./data/sequences/spike_align.aln}
-#' \dontrun{clustalRNA_load <- load_alignment(system.file('extdata',
-#'                                                        spike_align.aln',
-#'                                                        package='firstPackage'),
-#'                                            clustal')')}
+#' @details
+#' 1) spikeRNA_annot <- read.csv(system.file('extdata', 'spike_annot.csv',
+#'                               package='firstPackage'))
+#' 2) accessions <- spikeRNA_annot$accession
+#' 3) download manually each sequence from 'genbank' and save as spike.fasta and
+#'    perform an alignment with clustal omega with following commands
+#'    (it needs to be installed locally or use a web interface)
+#' 4) clustalo --threads=4 -v --outfmt=clustal -t RNA -i
+#'    ./data/sequences/spike.fasta -o ./data/sequences/spike_align.aln
+#' 5) clustalRNA_load <- load_alignment(system.file('extdata', 'spike_align.aln',
+#'                                      package='firstPackage'), 'clustal')
 #'
 'clustalRNA'
 
-#' Production and farm value of maple products in Canada
+#' alignment of RNA sequences for the spike protein of some sarbecoviruses
+#'   closely related to SARSCoV2 in fasta format
 #'
-#' @source Statistics Canada. Table 001-0008 - Production and farm value of
-#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
-#' @format A data frame with columns:
-#' \describe{
-#' \item{Year}{A value between 1924 and 2015.}
-#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
-#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
-#' \item{Region}{Postal code abbreviation for territory or province.}
-#' }
-#'  [1] "QHD43416.1"     "AAP30030.1"     "QHR63300.2"     "AGZ48831.1"     "YP_001039971.1" "ALK02457.1"     "AVP78031.1"     "AVP78042.1"     "AAY88866.1"     "AGZ48818.1"
-#'  [11] "AAS00003.1"     "AAP51227.1"     "AAU04646.1"     "YP_003858584.1"
-#' @examples
-#' \dontrun{
-#' maple
-#' }
+#' @description
+#' 14 different sequences of some sarbecoviruses closely related to SARSCoV2
+#'   were retrieved from public databases and compared using CLUSTAL omega (1.2.4)
+#'   alignment software with the output in fasta format (see details for
+#'   the complete protocol for retrieving, saving and processing the data)
+#'
+#' @source
+#'   the \href{https://www.ncbi.nlm.nih.gov/genbank/}{genbank} database of
+#'   the National Center for Biotechnology Information or the Universal Protein
+#'   Resource \href{https://www.uniprot.org/}{UniProt} database
+#'
+#' @format a matrix of class DNAbin
+#'  \describe{\item{attribute (dimnames)}{sequence name (accession number)}}
+#'
+#' @details
+#' 1) spikeRNA_annot <- read.csv(system.file('extdata', 'spike_annot.csv',
+#'                               package='firstPackage'))
+#' 2) accessions <- spikeRNA_annot$accession
+#' 3) download manually each sequence from 'genbank' and save as spike.fasta and
+#'    perform an alignment with clustal omega with following commands
+#'    (it needs to be installed locally or use a web interface)
+#' 4) clustalo --threads=4 -v --outfmt=fasta -t RNA -i
+#'    ./data/sequences/spike.fasta -o ./data/sequences/spike_align.fasta
+#' 5) fastaRNA_load <- load_alignment(system.file('extdata', 'spike_align.fasta',
+#'                                      package='firstPackage'), 'fasta')
+#'
 'fastaRNA'
 
-#' Production and farm value of maple products in Canada
+#' an example of alignment in mase format of related protein sequences from different taxa
 #'
-#' @source Statistics Canada. Table 001-0008 - Production and farm value of
-#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
-#' @format A data frame with columns:
-#' \describe{
-#' \item{Year}{A value between 1924 and 2015.}
-#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
-#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
-#' \item{Region}{Postal code abbreviation for territory or province.}
-#' }
-#' [1] "nb"  "nam" "seq" "com"
-#' @examples
-#' \dontrun{
-#' maple
-#' }
+#' @source
+#' the Universal Protein Resource \href{https://www.uniprot.org/}{UniProt} database
+#'
+#' @format a list of class alignment with 4 elements
+#' \describe{\item{nb}{number of sequences compared}
+#'           \item{nam}{taxon origen of sequence}
+#'           \item{seq}{a list of protein sequences}
+#'           \item{com}{NA}}
+#'
+#' @details
+#' 1) maseProtein_load <- load_alignment(system.file('extdata', 'prot.mase',
+#'                                      package='firstPackage'), 'mase')
+#' 2) data(maseProtein); stopifnot(identical(maseProtein, maseProtein_load))
+#'
 'maseProtein'
 
-#' Production and farm value of maple products in Canada
+#' alignment of RNA sequences for the replicase of some sarbecoviruses
+#'   closely related to SARSCoV2 in msf format
 #'
-#' @source Statistics Canada. Table 001-0008 - Production and farm value of
-#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
-#' @format A data frame with columns:
-#' \describe{
-#' \item{Year}{A value between 1924 and 2015.}
-#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
-#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
-#' \item{Region}{Postal code abbreviation for territory or province.}
-#' }
-#'  [1] "QHD43415_1"     "AAP30028_1"     "QHR63299_1"     "AGZ48830_1"     "YP_001039970_1" "ALK02468_1"     "AVP78030_1"     "AVP78041_1"     "AAY88865_2"     "AGZ48817_1"
-#'  [11] "AAS00002_1"     "AAP51225_1"     "AAU04645_1"     "YP_003858583_1"
-#' @examples
-#' \dontrun{
-#' maple
-#' }
+#' @description
+#' 14 different sequences of some sarbecoviruses closely related to SARSCoV2
+#'   were retrieved from public databases and compared using CLUSTAL omega (1.2.4)
+#'   alignment software with the output in msf format (see details for
+#'   the complete protocol for retrieving, saving and processing the data)
+#'
+#' @source
+#'   the \href{https://www.ncbi.nlm.nih.gov/genbank/}{genbank} database of
+#'   the National Center for Biotechnology Information or the Universal Protein
+#'   Resource \href{https://www.uniprot.org/}{UniProt} database
+#'
+#' @format a matrix of class DNAbin
+#'  \describe{\item{attribute (dimnames)}{sequence name (accession number)}}
+#'
+#' @details
+#' 1) orf1abRNA_annot <- read.csv(system.file('extdata', 'orf1ab_annot.csv',
+#'                               package='firstPackage'))
+#' 2) accessions <- orf1abRNA_annot$accession
+#' 3) download manually each sequence from 'genbank' and save as orf1ab.fasta and
+#'    perform an alignment with clustal omega with following commands
+#'    (it needs to be installed locally or use a web interface)
+#' 4) clustalo --threads=4 -v --outfmt=msf -t RNA -i
+#'    ./data/sequences/orf1ab.fasta -o ./data/sequences/orf1ab_align.msf
+#' 5) msfRNA_load <- load_alignment(system.file('extdata', 'orf1ab_align.msf',
+#'                                  package='firstPackage'), 'msf', 'RNA')
+#'
 'msfRNA'
-
-#' Production and farm value of maple products in Canada
-#'
-#' @source Statistics Canada. Table 001-0008 - Production and farm value of
-#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
-#' @format A data frame with columns:
-#' \describe{
-#' \item{Year}{A value between 1924 and 2015.}
-#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
-#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
-#' \item{Region}{Postal code abbreviation for territory or province.}
-#' }
-#' [1] "nb"  "nam" "seq" "com"
-#' @examples
-#' \dontrun{
-#' maple
-#' }
-'phylipProt'
-
-#' Production and farm value of maple products in Canada
-#'
-#' @source Statistics Canada. Table 001-0008 - Production and farm value of
-#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
-#' @format A data frame with columns:
-#' \describe{
-#' \item{Year}{A value between 1924 and 2015.}
-#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
-#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
-#' \item{Region}{Postal code abbreviation for territory or province.}
-#' }
-#'  [1] "QHD43416.1" "AAP30030.1" "QHR63300.2" "AGZ48831.1" "YP_0010399" "ALK02457.1" "AVP78031.1" "AVP78042.1" "AAY88866.1" "AGZ48818.1" "AAS00003.1" "AAP51227.1" "AAU04646.1"
-#'  [14] "YP_0038585"
-#' @examples
-#' \dontrun{
-#' maple
-#' }
-'phylipRNA'
