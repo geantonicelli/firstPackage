b643d3741e108934ef3b3d18529b422cd57db752
gerardo antonicelli <esteban.antonicelli@gmail.com>
2020-08-17 03:48:28 +0200

    spike.fasta to inst/exdata added dataset documentation

--- R/data.R
+++ R/data.R
@@ -1,19 +1,34 @@
-#' Production and farm value of maple products in Canada
+#' alignment of RNA sequences for the spike protein of some sarbecoviruses
+#' closely related to SARSCoV2
 #'
-#' @source Statistics Canada. Table 001-0008 - Production and farm value of
-#' maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
-#' @format A data frame with columns:
-#' \describe{
-#' \item{Year}{A value between 1924 and 2015.}
-#' \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
-#' \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
-#' \item{Region}{Postal code abbreviation for territory or province.}
-#' }
-#' [1] "nb"  "nam" "seq" "com"
+#' 14 different sequences of some sarbecoviruses closely related to SARSCoV2
+#' were retrieve from public databases and compared using CLUSTAL omega (1.2.4)
+#' alignment software (see examples section for the complete protocol for
+#' retrieving, saving and processing the data)
+#'
+#' @source the \href{https://www.ncbi.nlm.nih.gov/genbank/}{genbank} database of
+#'   the National Center for Biotechnology Information or the Universal Protein
+#'   Resource \href{https://www.uniprot.org/}{UniProt} database
+#'
+#' @format a list with 4 elements \describe{ \item{nb}{number of sequences
+#'   compared} \item{nam}{sequence name (accession number)} \item{seq}{a list of
+#'   RNA sequences} \item{com}{NA} }
+#'
 #' @examples
-#' \dontrun{
-#' maple
-#' }
+#' \dontrun{spikeRNA_annot <- read.csv(system.file('extdata',
+#'                                                'spike_annot.csv',
+#'                                                package='firstPackage'))}
+#' \dontrun{accessions <- spikeRNA_annot$accession}
+#' \dontrun{download manually each sequence from 'genbank' and save as spike.fasta}
+#' \dontrun{perform an alignment with clustal omega with following commands
+#'   (it needs to be installed locally or use a web interface)}
+#' \dontrun{clustalo --threads=4 -v --outfmt=clustal -t RNA -i
+#'          ./data/sequences/spike.fasta -o ./data/sequences/spike_align.aln}
+#' \dontrun{clustalRNA_load <- load_alignment(system.file('extdata',
+#'                                                        spike_align.aln',
+#'                                                        package='firstPackage'),
+#'                                            clustal')')}
+#'
 'clustalRNA'
 
 #' Production and farm value of maple products in Canada
