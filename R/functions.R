623cbad2b87d49e1a72c3ad8edb8af0c69206f31
gerardo antonicelli <esteban.antonicelli@gmail.com>
2020-08-21 00:53:27 +0200

    new function added max_likelihood()

--- R/functions.R
+++ R/functions.R
@@ -286,14 +286,14 @@ load_alignment <- function(file, format, type='protein'){
                            return(alignment)
                            }
 
-#' function to calculate a phylogenetic tree
+#' function to calculate a distance-based phylogenetic tree with bootstrapping validation
 #'
 #' this function is a wrapper around the functions
 #'   \code{'\link[seqinr]{dist.alignment}'}, \code{'\link[ape]{dist.dna}'},
 #'   \code{'\link[ape]{nj}'}, \code{'\link[ape]{bionj}'},
 #'   \code{'\link[ape]{fastme.bal}'}, \code{'\link[ape]{fastme.ols}'},
 #'   \code{'\link[ape]{boot.phylo}'} and \code{'\link[ape]{plot.phylo}'}. it takes
-#'   a sequences alignment in format alignment of DNAbin matrix and perform all
+#'   a sequences alignment in format 'alignment' of 'DNAbin' matrix and perform all
 #'   transformations and steps to calculate a phylogenetic distance matrix based
 #'   on similarity or identity in the case of proteins or based in evolutionary
 #'   models in the case of DNA or RNA, to perform a distance-based phylogenetic
@@ -345,7 +345,7 @@ load_alignment <- function(file, format, type='protein'){
 #' clustalRNATree <- make_tree(clustalRNA, type=RNA)
 #'
 #' @importFrom seqinr dist.alignment as.matrix.alignment
-#' @importFrom ape as.alignment as.DNAbin dist.dna nj bionj fastme.bal
+#' @importFrom ape as.alignment as.DNAbin dist.dna nj bionj njs bionjs fastme.bal
 #'   fastme.ols makeLabel ladderize root boot.phylo plot.phylo nodelabels
 #'
 #' @export
@@ -383,18 +383,18 @@ make_tree <- function(alignment, type, model=TN93, clustering=bionj, outgroup=NU
                       return(mytree)
                       }
 
-#' function to calculate a phylogenetic tree
+#' function to calculate a maximum pasimony phylogenetic tree
 #'
 #' this function is a wrapper around the functions
 #'   \code{'\link[seqinr]{dist.alignment}'}, \code{'\link[ape]{dist.dna}'},
 #'   \code{'\link[ape]{nj}'}, \code{'\link[ape]{bionj}'},
-#'   \code{'\link[ape]{fastme.bal}'}, \code{'\link[ape]{fastme.ols}'},
+#'   \code{'\link[ape]{fastme.bal}'}, \code{'\link[ape]{fastme.ols}'} and
 #'   \code{'\link[phangorn]{optim.parsimony}'}. it takes a sequences alignment in
-#'   format alignment of DNAbin matrix and perform all transformations and steps
-#'   to calculate a phylogenetic distance matrix based on similarity or identity
-#'   in the case of proteins or based in evolutionary models in the case of DNA or
-#'   RNA, to perform a phylogenetic clustering and to optimise the phylogeny by a
-#'   maximum parsimony based method
+#'   format 'alignment' of 'DNAbin' matrix and perform all transformations and
+#'   steps to calculate a phylogenetic distance matrix based on similarity or
+#'   identity in the case of proteins or based in evolutionary models in the case
+#'   of DNA or RNA, to perform a phylogenetic clustering and to optimise the
+#'   phylogeny by a maximum parsimony algorithm
 #'
 #' @param alignment an object of class alignment or DNAbin containing a DNA, RNA
 #'   or protein sequences alignment
@@ -410,7 +410,7 @@ make_tree <- function(alignment, type, model=TN93, clustering=bionj, outgroup=NU
 #'   as root for building the tree. The default value is NULL leading to an
 #'   unrooted tree
 #'
-#' @return the function returns an object of class phylo of the 'ape' package.
+#' @return the function returns an object of class 'phylo' of the 'ape' package.
 #'   Advanced and elaborated plots can be drawn in later steps based on the tree
 #'   data of the phylo class object
 #'
@@ -438,8 +438,8 @@ make_tree <- function(alignment, type, model=TN93, clustering=bionj, outgroup=NU
 #' \dontrun{plot.phylo(phylipRNAtree, type='u')}
 #'
 #' @importFrom seqinr dist.alignment
-#' @importFrom ape as.DNAbin dist.dna nj bionj fastme.bal fastme.ols makeLabel
-#'   ladderize root
+#' @importFrom ape as.DNAbin dist.dna nj bionj njs bionjs fastme.bal fastme.ols
+#'   makeLabel ladderize root
 #' @importFrom phangorn as.phyDat optim.parsimony
 #'
 #' @export
@@ -464,3 +464,154 @@ max_parsimony <- function(alignment, type, clustering=bionj, outgroup=NULL){
                           else{my_tree <- root(optimtree, outgroup=out, r=TRUE)}
                           return(my_tree)
                           }
+
+#' function to calculate a maximum likelihood phylogenetic tree
+#'
+#' this function is a wrapper around the functions
+#' \code{'\link[seqinr]{dist.alignment}'}, \code{'\link[ape]{dist.dna}'},
+#' \code{'\link[ape]{nj}'}, \code{'\link[ape]{bionj}'},
+#' \code{'\link[ape]{fastme.bal}'}, \code{'\link[ape]{fastme.ols}'},
+#' \code{'\link[phangorn]{pml}'} and \code{'\link[phangorn]{optim.pml}'}. it takes a sequences alignment in
+#' format 'alignment' of 'DNAbin' matrix and perform all transformations and steps
+#' to calculate a phylogenetic distance matrix based on similarity or identity
+#' in the case of proteins or based in evolutionary models in the case of DNA or
+#' RNA, to perform a likelihood-based phylogenetic clustering and to optimise the phylogeny by a
+#' maximum likelihood algorithm
+#'
+#' @param alignment an object of class alignment or DNAbin containing a DNA, RNA
+#'   or protein sequences alignment
+#' @param type a character string without '' specifying the type of sequences,
+#'   i.e. DNA, RNA or protein (without default value)
+#' @param model a character string without '' specifying the model to be used
+#'   for the calculation of the distances matrix, i.e. raw, N, TS, TV, JC69,
+#'   K80, F81, K81, F84, BH87, T92, TN93 (default), GG95, logdet, paralin,
+#'   indel, or indelblock
+#' @param clustering a character string without '' specifying the clustering
+#'   algorithm to be used to build the phylogenetic tree, i.e classic
+#'   neighbor-joining (nj), improved neighbor-joining (bionj) (default), NJ or
+#'   bio-NJ from a distance matrix with possibly missing values (njs, bionjs),
+#'   balanced minimum evolution principle (fastme.bal), ordinary least-squares
+#'   minimum evolution principle (fastme.ols)
+#' @param pml.model a character string without '' specifying the model to be
+#'   used for the calculation and optimisation of the maximum likelihood, for
+#'   nucleotide sequences they are 23, i.e. JC, F81, K80, HKY, TrNe, TrN, TPM1,
+#'   K81, TPM1u, TPM2, TPM2u, TPM3, TPM3u, TIM1e, TIM1, TIM2e, TIM2, TIM3e,
+#'   TIM3, TVMe, TVM, SYM, GTR, whereas for protein sequences there are 17
+#'   algorithms available, i.e. WAG, JTT, LG, Dayhoff, cpREV, mtmam, mtArt,
+#'   MtZoa, mtREV24, VT, RtREV, HIVw, HIVb, FLU, Blosum62, Dayhoff_DCMut,
+#'   JTT_DCMut. The default model corresponds to the model F81 for nucleotides
+#'   and it is set up with the parameters 'optBf=TRUE' and 'optQ=FALSE' in the
+#'   function 'optim.pml()' of the phangorn package inside the function
+#'   'max_likelihood()'. It is possible to run all the models at once for a
+#'   given type of sequence with the value 'all' without ''
+#' @param outgroup a character string without '' specifying a taxon to be taken
+#'   as root for building the tree. The default value is NULL leading to an
+#'   unrooted tree
+#' @param clean logical (default=TRUE) indicating if the input sequences should
+#'   be cleaned of any gap position or undefined nucleotide or aminoacid residue
+#'   to improve philogenetic prediction, set to FALSE if you want to clean the
+#'   sequences more gently and flexibly with 'clean_alignment' before
+#'   calculations or if you want to use the raw sequences alignment
+#'
+#' @return the function returns an object of class 'pml' of the 'phangorn'
+#'   package. Advanced and elaborated plots can be drawn in later steps based on
+#'   the tree data of the pml class object
+#'
+#' @author gerardo esteban antonicelli
+#'
+#' @seealso \code{'\link{retrieve_seqs}'} \code{'\link{print_alignment}'}
+#'   \code{'\link{clean_alignment}'} \code{'\link{load_alignment}'}
+#'   \code{'\link{make_tree}'} \code{'\link{max_parsimony}'}
+#'
+#' @aliases \alias{max_likelihood}
+#'
+#' @examples
+#' data(fastaRNA)
+#' data(phylipRNA)
+#' data(phylipProt)
+#' clustalRNA <- load_alignment(system.file('extdata',
+#'                                          'spike_align.aln',
+#'                                          package='firstPackage'),
+#'                              'clustal')
+#' fastaRNAtree <- max_parsimony(alignment=fastaRNA, type=RNA,
+#'                               clustering=fastme.ols)
+#' phylipRNAtree <- max_parsimony(phylipRNA, RNA, fastme.bal)
+#' phylipProtTree <- max_parsimony(phylipProt, protein, outgroup=YP_0010399)
+#' clustalRNAtree <- max_parsimony(clustalRNA, type=RNA, clustering=bionj)
+#' \dontrun{plot.phylo(phylipRNAtree, type='u')}
+#'
+#' @importFrom seqinr dist.alignment as.matrix.alignment
+#' @importFrom ape as.alignment as.DNAbin dist.dna nj bionj fastme.bal
+#'   fastme.ols root
+#' @importFrom phangorn as.phyDat pml optim.pml
+#'
+#' @export
+max_likelihood <- function(alignment, type, model=TN93, clustering=bionj, pml.model=NULL, outgroup=NULL, clean=TRUE){
+                           seqtype <- deparse(substitute(type))
+                           distmodel <- deparse(substitute(model))
+                           pmlmodel <- deparse(substitute(pml.model))
+                           if(pmlmodel=='NULL'){pmlmodel <- NULL}
+                           out <- deparse(substitute(outgroup))
+                           if(seqtype=='protein'){
+                              matrix <- toupper(as.matrix.alignment(alignment))
+                              if(clean==TRUE){
+                                 na.posi <- which(apply(matrix, 2, function(e) any(!e %in% c('G', 'A', 'L', 'M', 'F',
+                                                                                             'W', 'K', 'Q', 'E', 'S',
+                                                                                             'P', 'V', 'I', 'C', 'Y',
+                                                                                             'H', 'R', 'N', 'D', 'T'))))
+                                 cleaned <- matrix[ , -na.posi]
+                                 }
+                              else{cleaned <- matrix
+                                  }
+                              phyDat <- as.phyDat(cleaned, type='AA')
+                              mydist <- dist.alignment(as.alignment(cleaned))
+                              }
+                           else if(seqtype=='DNA'|| seqtype=='RNA'){
+                                   if(class(alignment)[[1]]=='DNAbin'){DNAbin <- alignment}
+                                   else{DNAbin <- as.DNAbin(alignment)}
+                                   if(clean==TRUE){
+                                      na.posi <- which(apply(as.character(DNAbin), 2, function(e) any(!e %in% c('a', 't', 'g', 'c'))))
+                                      DNAbinclean <- DNAbin[ , -na.posi]
+                                      }
+                                   else{DNAbinclean <- DNAbin}
+                                   phyDat <- as.phyDat(DNAbinclean)
+                                   mydist <- dist.dna(DNAbinclean, model=distmodel)
+                                   }
+                           mytree <- eval(substitute(clustering(mydist)))
+                           if(!is.null(pmlmodel) && pmlmodel=='all'){
+                              my_tree <- list()
+                              if(seqtype=='protein'){
+                                 models <- c('WAG', 'JTT', 'LG', 'Dayhoff', 'cpREV',
+                                             'mtmam', 'mtArt', 'MtZoa', 'mtREV24', 'VT',
+                                             'RtREV', 'HIVw', 'HIVb', 'FLU', 'Blosum62',
+                                             'Dayhoff_DCMut', 'JTT_DCMut')
+                                 for(i in 1:length(models)){
+                                     fitted <- pml(mytree, phyDat, k=4, model=models[[i]])
+                                     optim <- optim.pml(fitted, optNni=TRUE, optGamma=TRUE, model=models[[i]])
+                                     my_tree[[i]] <- optim
+                                     names(my_tree)[[i]] <- models[[i]]
+                                     }
+                                 }
+                              else if(seqtype=='DNA'|| seqtype=='RNA'){
+                                      models <- c('JC', 'F81', 'K80', 'HKY', 'TrNe',
+                                                  'TrN', 'TPM1', 'K81', 'TPM1u', 'TPM2',
+                                                  'TPM2u', 'TPM3', 'TPM3u', 'TIM1e', 'TIM1',
+                                                  'TIM2e', 'TIM2', 'TIM3e', 'TIM3', 'TVMe',
+                                                  'TVM', 'SYM', 'GTR')
+                                      for(i in 1:length(models)){
+                                          fitted <- pml(mytree, phyDat, k=4, model=models[[i]])
+                                          optim <- optim.pml(fitted, optNni=TRUE, optBf=TRUE, optQ=TRUE, optGamma=TRUE, model=models[[i]])
+                                          my_tree[[i]] <- optim
+                                          names(my_tree)[[i]] <- models[[i]]
+                                          }
+                                      }
+                              }
+                           else{fittedtree <- pml(mytree, phyDat, k=4, model=pmlmodel)
+                                if(out=='NULL'){my_tree <- optim.pml(fittedtree, optNni=TRUE, optBf=TRUE, optGamma=TRUE, model=pmlmodel)
+                                   }
+                                else{fittedtree$tree <- root(fittedtree$tree, outgroup=out, r=TRUE)
+                                     my_tree <- optim.pml(fittedtree, optNni=TRUE, optBf=TRUE, optGamma=TRUE, optRooted=TRUE, model=pmlmodel)
+                                     }
+                                }
+                           return(my_tree)
+                           }
